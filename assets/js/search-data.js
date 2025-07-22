9137ca075aa56fd7fb531ef11519bd248b952141
GVLevesque <156103862+GVLevesque@users.noreply.github.com>
2025-07-22 00:06:37 +0000

    Deploying to gh-pages from @ GVLevesque/gvlevesque.github.io@85cd70f3f8dff8a15a2189079ead7f7c39afa221 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -31,8 +31,8 @@ ninja.data = [{
             window.location.href = "/cv/";
           },
         },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          title: "Teaching",
+          description: "I have had the pleasure to serve as an instructor at three universities in the past few years. Here&#39;s an overview of the courses I taught.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
