7730703717f129119806cbfa0e12832031deeb15
GVLevesque <156103862+GVLevesque@users.noreply.github.com>
2025-07-22 00:38:06 +0000

    Deploying to gh-pages from @ GVLevesque/gvlevesque.github.io@e9357826e800572798b9f33083486dd42b555a1b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -17,7 +17,7 @@ ninja.data = [{
             window.location.href = "/publications/";
           },
         },{id: "nav-projects",
-          title: "projects",
+          title: "Projects",
           description: "Here are some of the projects I&#39;m currently working on.",
           section: "Navigation",
           handler: () => {
