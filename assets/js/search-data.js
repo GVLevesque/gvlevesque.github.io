b5d5ac10acbe417cb9e2d081778bbd3f4704ab6b
GVLevesque <156103862+GVLevesque@users.noreply.github.com>
2025-07-27 17:53:51 +0000

    Deploying to gh-pages from @ GVLevesque/gvlevesque.github.io@da6e23690965f80575552b1d0f355db84c0b1262 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -52,9 +52,9 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project.html";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-regulatory-power-over-the-mitigation-of-occupational-health-risks-in-canada-and-the-united-states",
+          title: 'Regulatory power over the mitigation of occupational health risks in Canada and the...',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project.html";
             },},{id: "projects-project-4",
